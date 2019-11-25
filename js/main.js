7f034617aca9266f78499b96fe9e4560dcb5b462
laole918 <laole918@qq.com>
2019-11-25 11:15:29 +0800

    test touch event.

--- js/main.js
+++ js/main.js
@@ -3,8 +3,8 @@
     var WEB_W = window.innerWidth, WEB_H = window.innerHeight;
     console.log("WEB_W:" + WEB_W + ",WEB_H:" + WEB_H);
     var C_W = 480, C_H = 320;
-    C_W = Math.round(C_H * WEB_W / WEB_H);
-    // C_H = Math.round(C_W * WEB_H / WEB_W);
+    // C_W = Math.round(C_H * WEB_W / WEB_H);
+    C_H = Math.round(C_W * WEB_H / WEB_W);
     console.log("C_W:" + C_W + ",C_H:" + C_H);
     canvas.width = C_W;
     canvas.height = C_H;
@@ -12,17 +12,56 @@
     document.body.appendChild(canvas);
     var ctx = canvas.getContext("2d");
     var lastTime = 0;
+    
+    function checkButton(x, y, w, h, tx, ty) {
+        if(tx >= x && tx <= x + w && ty >= y && ty <= y + h) {
+            return true;
+        }
+        return false;
+    }
+
+    var button1 = {
+    };
+    button1.id = -1;
+    var button2 = {
+    };
+    button2.id = -1;
 
     function onTouchEvent(touchEvent) {
         var event = touchEvent || window.event;
-        switch (event.type) {
-            case "touchstart":
-                console.log(event.touches);
-                break;
-            case "touchend":
-                break;
-            case "touchmove":
-                break;
+        var touches = event.changedTouches;
+        for (var i = 0; i < touches.length; i ++) {
+            var touche = touches[i];
+            switch (event.type) {
+                case "touchstart":
+                    // console.log(event.type);
+                    // console.log(touche);
+                    console.log(event.type + " " + touche.identifier);
+                    var tx = touche.clientX * C_W / WEB_W;
+                    var ty = touche.clientY * C_H / WEB_H;
+                    if(checkButton(80, 80, 100, 100, tx, ty)) {
+                        button1.id = touche.identifier;
+                    }
+                    if(checkButton(80, 200, 100, 100, tx, ty)) {
+                        button2.id = touche.identifier;
+                    }
+                    break;
+                case "touchend":
+                    // console.log(event.type);
+                    // console.log(touches);
+                    console.log(event.type + " " + touche.identifier);
+                    if(touche.identifier === button1.id) {
+                        button1.id = -1;
+                    }
+                    if(touche.identifier === button2.id) {
+                        button2.id = -1;
+                    }
+                    break;
+                case "touchmove":
+                    // console.log(event.type);
+                    // console.log(touches);
+                    break;
+            }
         }
     }
 
@@ -34,6 +73,20 @@
         ctx.fillStyle = "white";
         ctx.font = "48px serif";
         ctx.fillText("曹大人你好！", 0, 48);
+        // console.log(button1.id);
+        if(button1.id < 0) {
+            ctx.fillStyle = "red";
+        } else {
+            ctx.fillStyle = "green";
+        }
+        ctx.fillRect(80, 80, 100, 100);
+        console.log(button2.id);
+        if(button2.id < 0) {
+            ctx.fillStyle = "red";
+        } else {
+            ctx.fillStyle = "green";
+        }
+        ctx.fillRect(80, 200, 100, 100);
     }
 
     function update() {
